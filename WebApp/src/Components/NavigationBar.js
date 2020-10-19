860d73ac226441b167782e3b506191686869876c
mamathaguntu <mamathaguntu@gmail.com>
2020-10-19 15:59:21 -0700

    Create NavigationBar.js

---
+++ WebApp/src/Components/NavigationBar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import {Navbar, Nav} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
+class NavigationBar extends Component {
+    render() {
+        return (
+            
+           <Navbar bg = 'dark' variant = 'dark'>
+                <Link to = {""} className="navbar-brand">File Storage</Link>
+                <Nav className="mr-auto">
+                {/* <Link to={"login"} className="nav-link">Login Page</Link> */}
+                {/* <Link to={"register"} className="nav-link">Registration</Link> */}
+                </Nav>
+            </Navbar>
+        )
+    }
+}
+
+export default NavigationBar
