65689f2a1ef9ed413fc06ef554cb2e724878ebd6
mamathaguntu <mamathaguntu@gmail.com>
2020-10-20 00:57:57 -0700

    Create UserDataDao.java

---
+++ ServerCode/src/main/java/com.sjsu/repo/UserDataDao.java
@@ -0,0 +1,9 @@
+package com.sjsu.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sjsu.Entity.UserData;
+
+public interface UserDataDao extends JpaRepository<UserData, Integer > {
+
+}
