69df8705695d2cd96af583e582bfe7e2a813ded0
mamathaguntu <mamathaguntu@gmail.com>
2020-10-23 11:40:16 -0700

    Update App.js

--- WebApp/src/App.js
+++ WebApp/src/App.js
@@ -4,10 +4,12 @@ import NavigationBar from './Components/NavigationBar';
 import {Container, Row, Col, Jumbotron} from 'react-bootstrap'
 import Footer  from "./Components/Footer";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import LoginPage from './Components/LoginPage'
 import WelcomePage from './Components/WelcomePage';
 import RegistrationPage from './Components/RegistrationPage';
 import AdminSrcPage from './Components/AdminSrcPage';
+import DisplayMyFiles from './Components/DisplayMyFiles'
+import DisplayUserDetails from './Components/DisplayUserDetails';
+import LoginP from './Components/LoginP'
 
 function App() {
   const marginTop = {
@@ -22,13 +24,17 @@ function App() {
         <Col lg = {12} style = {marginTop}>
         <Switch>
           <Route path="/" exact component={WelcomePage} />
-          <Route path="/login" exact component={LoginPage} />
+          <Route path="/login" exact component={LoginP} />
           <Route path="/register" exact component={RegistrationPage} />
           <Route path="/adminPage" exact component={AdminSrcPage} />
+          <Route path="/displayMyFiles" exact component={DisplayMyFiles} />
+          <Route path="/users" exact component={DisplayUserDetails} />
+         
         </Switch>
         </Col>
       </Row>
     </Container>
+    <testDataPage/>
     <Footer />
     </Router>
     
