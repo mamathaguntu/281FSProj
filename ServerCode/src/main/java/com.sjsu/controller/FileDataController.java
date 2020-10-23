9c6d20d60fbb4d3cffb29bb5ba96f38a6a3e3265
mamathaguntu <mamathaguntu@gmail.com>
2020-10-22 20:18:54 -0700

    Create FileDataController.java

---
+++ ServerCode/src/main/java/com.sjsu/controller/FileDataController.java
@@ -0,0 +1,54 @@
+package com.sjsu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import com.sjsu.Entity.FileData;
+import com.sjsu.repo.FileDataDao;
+import java.util.List;
+
+@RestController
+@RequestMapping("/FileData")
+@CrossOrigin(origins="http://localhost:3000")
+public class FileDataController {
+
+	 @Autowired
+	 private FileDataDao fda;
+	 
+	 @PostMapping("/post/fileData" )
+	 public FileData addFile(@RequestBody FileData fileData) {
+		 FileData data1 = fda.save(fileData);
+		return data1;		 
+	 }
+	 
+	 @GetMapping("/getFiles/{id}")
+	 public FileData getFile(@PathVariable int id) {
+		 FileData dataa1 = fda.getOne(id);
+		 return dataa1;	 
+	 }
+	 
+	 @GetMapping("/getAllFiles")
+	 public List<FileData> getAllFiles() {
+		 List<FileData> dataa1 = fda.findAll();
+		 return dataa1;	 
+	 }
+	 	 
+	 @DeleteMapping("/deleteFile/{fileID}")
+	 public void removeFileById(@PathVariable("fileId") int fileId){
+		 fda.deleteById(fileId);
+	 }
+	 
+	 @PutMapping("/editFile/fileData")
+	 public void updateFile(@RequestBody FileData fileData){ 
+		 fda.save(fileData);
+	 }
+	 
+}
