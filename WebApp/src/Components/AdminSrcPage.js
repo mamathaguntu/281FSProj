67a57b8a907ccfa139f33ed36f9db6a90833a14e
mamathaguntu <mamathaguntu@gmail.com>
2020-10-25 02:21:46 -0700

    Update AdminSrcPage.js

--- WebApp/src/Components/AdminSrcPage.js
+++ WebApp/src/Components/AdminSrcPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Tab, Nav, Col, Row} from 'react-bootstrap'
 import Tabs from 'react-bootstrap/Tabs'
 import DisplayUserDetails from './DisplayUserDetails'
-import DisplayMyFiles from './DisplayMyFiles'
+import DisplayAllFiles from './DisplayAllFiles'
 import moduleName from './LoginPage'
 import LogoutPage from './LogoutPage'
 
@@ -19,9 +19,6 @@ function AdminSrcPage() {
                     <Nav.Item>
                     <Nav.Link eventKey="second">Files</Nav.Link>
                     </Nav.Item>
-                    {/* <Nav.Item>
-                    <Nav.Link eventKey="third">Logout</Nav.Link>
-                    </Nav.Item> */}
                 </Nav>
                 </Col>
                 <Col sm={9}>
@@ -30,7 +27,7 @@ function AdminSrcPage() {
                     <DisplayUserDetails />
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
-                    <DisplayMyFiles/>
+                    <DisplayAllFiles/>
                     </Tab.Pane>
                 </Tab.Content>
                 </Col>
