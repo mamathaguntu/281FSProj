1c958d02049faa950d47fb3b69431ce6b153f682
mamathaguntu <mamathaguntu@gmail.com>
2020-10-25 02:28:26 -0700

    Update NavigationBar.js

--- WebApp/src/Components/NavigationBar.js
+++ WebApp/src/Components/NavigationBar.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react'
 
 import {Navbar, Nav} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import CloudIcon from '@material-ui/icons/Cloud';
 
 class NavigationBar extends Component {
     render() {
         return (
             
            <Navbar bg = 'dark' variant = 'dark'>
-                <Link to = {""} className="navbar-brand">File Storage</Link>
-                <Nav className="mr-auto">
-                <Link to={"login"} className="nav-link">Login</Link>
-                </Nav>
+                <Link to = {""} className="navbar-brand"> <CloudIcon/>File Storage</Link>
             </Navbar>
         )
     }
