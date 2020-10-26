9ddf4f8f6cdfec1987dae8dec845d670282817b8
mamathaguntu <mamathaguntu@gmail.com>
2020-10-26 08:55:21 -0700

    Update S3Controller.java

--- ServerCode/src/main/java/com.sjsu/controller/S3Controller.java
+++ ServerCode/src/main/java/com.sjsu/controller/S3Controller.java
@@ -1,19 +1,25 @@
 package com.sjsu.controller;
 
 
+import java.io.File;
 import java.util.List;
 
+import org.apache.tomcat.util.http.fileupload.FileItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.sjsu.Service.S3Services;
 
 @RestController
+@CrossOrigin(origins="*")
 public class S3Controller {
 	
 	@Autowired
@@ -24,9 +30,19 @@ public class S3Controller {
 	  return s3Services.listFiles();
 	}
 	
+	@GetMapping("/GetFileByFileName/{fileName}")
+	public boolean getFileByFileName(@PathVariable String fileName) {
+		return s3Services.getFileByFileName(fileName);
+	}
+	
 	@GetMapping("/listFileDetails")
 	public List<S3ObjectSummary> listFileDetails(){
 	  return s3Services.listFileDetails();
+	}
+	
+	@GetMapping("/listMyFileDetails/{email}")
+	public List<S3ObjectSummary> listMyFileDetails(@PathVariable String email){
+	  return s3Services.listMyFileDetails(email);
 	}
 	
 	@DeleteMapping("/deleteFile/{fileName}")
@@ -39,15 +55,19 @@ public class S3Controller {
       return "Successfully deleted -> FileName = " + fileName;
     }    
 	
-	@PostMapping("/uploadFile/{fileName}")
-	 public String uploadFile(String fileName, String filePath) {
+	@PostMapping("/uploadFile")
+	 public String uploadFile(@RequestParam("file") MultipartFile file) {
+	
+		String keyName = "";
 		try {
-			s3Services.uploadFile(fileName, filePath);
+			 keyName = file.getOriginalFilename();
+			 
+			s3Services.uploadFile(keyName, file);
 		}
       	catch (Exception e) {
-      		return "Unable to upload the file" + fileName;
+      		return "Unable to upload the file" + keyName;
 		}
-        return "File" + fileName + ", uploaded successfully";
+        return "File" + keyName + ", uploaded successfully";
     }
 	
 	@GetMapping("/downloadFile/{fileName}")
